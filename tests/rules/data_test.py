8d0c41e47cf90d38ac205f5ca4ead68a820eb2e3
Greg Pauloski <18683347+gpauloski@users.noreply.github.com>
2022-07-18 15:33:27 -0500

    Make rule extension global for guild

--- tests/rules/data_test.py
+++ tests/rules/data_test.py
@@ -3,26 +3,25 @@ from __future__ import annotations
 import os
 import pathlib
 
-from threepseat.rules.data import ChannelConfig
+from threepseat.rules.data import GuildConfig
 from threepseat.rules.data import Rules
 from threepseat.rules.data import UserOffenses
 
-CHANNEL_CONFIG = ChannelConfig(
+GUILD_CONFIG = GuildConfig(
     guild_id=1234,
-    channel_id=5678,
+    enabled=1,
     event_expectancy=0.5,
     event_duration=24,
     event_cooldown=5.0,
     last_event=0,
     max_offenses=3,
     timeout_duration=300,
-    prefixes='3pseat 3pfeet',
+    prefixes='3pseat, 3pfeet',
 )
 
 
 USER_OFFENSES = UserOffenses(
     guild_id=1234,
-    channel_id=5678,
     user_id=9012,
     current_offenses=0,
     total_offenses=0,
@@ -37,13 +36,13 @@ def test_makes_parent_dirs(tmp_path: pathlib.Path) -> None:
     assert os.path.isdir(db_parent_path)
 
 
-def test_channel_configs_table_created(tmp_file: str) -> None:
+def test_configs_table_created(tmp_file: str) -> None:
     rules = Rules(db_path=tmp_file)
 
     with rules.connect() as db:
         res = db.execute(
             'SELECT COUNT(*) FROM sqlite_master '
-            'WHERE type="table" AND name="channel_configs"',
+            'WHERE type="table" AND name="guild_configs"',
         ).fetchone()
         assert res[0] == 1
 
@@ -62,29 +61,29 @@ def test_user_offenses_table_created(tmp_file: str) -> None:
 def test_config(tmp_file: str) -> None:
     rules = Rules(db_path=tmp_file)
 
-    config = CHANNEL_CONFIG._replace(guild_id=1, channel_id=10)
+    config = GUILD_CONFIG._replace(guild_id=1)
     rules.update_config(config)
-    found_config = rules.get_config(guild_id=1, channel_id=10)
+    found_config = rules.get_config(guild_id=1)
     assert config == found_config
 
-    config = CHANNEL_CONFIG._replace(guild_id=2, channel_id=11)
+    config = GUILD_CONFIG._replace(guild_id=2)
     rules.update_config(config)
-    found_config = rules.get_config(guild_id=2, channel_id=11)
+    found_config = rules.get_config(guild_id=2)
     assert config == found_config
 
-    assert rules.get_config(guild_id=3, channel_id=12) is None
+    assert rules.get_config(guild_id=3) is None
 
 
 def test_update_config(tmp_file: str) -> None:
     rules = Rules(db_path=tmp_file)
 
-    config = CHANNEL_CONFIG._replace(guild_id=1, channel_id=10)
+    config = GUILD_CONFIG._replace(guild_id=1)
     rules.update_config(config)
 
     with rules.connect() as db:
         rows = db.execute(
-            'SELECT * FROM channel_configs '
-            'WHERE channel_id = 10 AND max_offenses = 3',
+            'SELECT * FROM guild_configs '
+            'WHERE guild_id = 1 AND max_offenses = 3',
         ).fetchall()
         assert len(rows) == 1
 
@@ -92,16 +91,31 @@ def test_update_config(tmp_file: str) -> None:
 
     with rules.connect() as db:
         rows = db.execute(
-            'SELECT * FROM channel_configs'
-            ' WHERE channel_id = 10 AND max_offenses = 3',
+            'SELECT * FROM guild_configs'
+            ' WHERE guild_id = 1 AND max_offenses = 3',
         ).fetchall()
         assert len(rows) == 0
 
         rows = db.execute(
-            'SELECT * FROM channel_configs '
-            'WHERE channel_id = 10 AND max_offenses = 5',
+            'SELECT * FROM guild_configs '
+            'WHERE guild_id = 1 AND max_offenses = 5',
         ).fetchall()
         assert len(rows) == 1
+
+
+def test_get_configs(tmp_file: str) -> None:
+    rules = Rules(db_path=tmp_file)
+
+    assert len(rules.get_configs()) == 0
+
+    rules.update_config(GUILD_CONFIG._replace(guild_id=1))
+    rules.update_config(GUILD_CONFIG._replace(guild_id=2))
+    rules.update_config(GUILD_CONFIG._replace(guild_id=3))
+    rules.update_config(GUILD_CONFIG._replace(guild_id=4))
+
+    configs = rules.get_configs()
+    assert len(configs) == 4
+    assert {config.guild_id for config in configs} == {1, 2, 3, 4}
 
 
 def test_user(tmp_file: str) -> None:
@@ -109,27 +123,27 @@ def test_user(tmp_file: str) -> None:
 
     user = USER_OFFENSES._replace(user_id=1)
     rules.update_user(user)
-    found_user = rules.get_user(guild_id=1234, channel_id=5678, user_id=1)
+    found_user = rules.get_user(guild_id=1234, user_id=1)
     assert user == found_user
 
     user = USER_OFFENSES._replace(user_id=2)
     rules.update_user(user)
-    found_user = rules.get_user(guild_id=1234, channel_id=5678, user_id=2)
+    found_user = rules.get_user(guild_id=1234, user_id=2)
     assert user == found_user
 
-    assert rules.get_user(guild_id=1, channel_id=1, user_id=1) is None
+    assert rules.get_user(guild_id=1, user_id=1) is None
 
 
 def test_update_user(tmp_file: str) -> None:
     rules = Rules(db_path=tmp_file)
 
-    user = USER_OFFENSES._replace(user_id=1)
+    user = USER_OFFENSES._replace(guild_id=5678, user_id=1)
     rules.update_user(user)
 
     with rules.connect() as db:
         rows = db.execute(
             'SELECT * FROM user_offenses '
-            'WHERE channel_id = 5678 AND user_id = 1 AND current_offenses = 0',
+            'WHERE guild_id = 5678 AND user_id = 1 AND current_offenses = 0',
         ).fetchall()
         assert len(rows) == 1
 
@@ -138,13 +152,13 @@ def test_update_user(tmp_file: str) -> None:
     with rules.connect() as db:
         rows = db.execute(
             'SELECT * FROM user_offenses '
-            'WHERE channel_id = 5678 AND user_id = 1 AND current_offenses = 0',
+            'WHERE guild_id = 5678 AND user_id = 1 AND current_offenses = 0',
         ).fetchall()
         assert len(rows) == 0
 
         rows = db.execute(
             'SELECT * FROM user_offenses '
-            'WHERE channel_id = 5678 AND user_id = 1 AND current_offenses = 1',
+            'WHERE guild_id = 5678 AND user_id = 1 AND current_offenses = 1',
         ).fetchall()
         assert len(rows) == 1
 
@@ -152,112 +166,117 @@ def test_update_user(tmp_file: str) -> None:
 def test_get_users(tmp_file: str) -> None:
     rules = Rules(db_path=tmp_file)
 
-    rules.update_user(USER_OFFENSES._replace(channel_id=1, user_id=1))
-    rules.update_user(USER_OFFENSES._replace(channel_id=1, user_id=2))
-    rules.update_user(USER_OFFENSES._replace(channel_id=1, user_id=3))
-    rules.update_user(USER_OFFENSES._replace(channel_id=2, user_id=1))
+    rules.update_user(USER_OFFENSES._replace(guild_id=1, user_id=1))
+    rules.update_user(USER_OFFENSES._replace(guild_id=1, user_id=2))
+    rules.update_user(USER_OFFENSES._replace(guild_id=1, user_id=3))
+    rules.update_user(USER_OFFENSES._replace(guild_id=2, user_id=1))
 
-    users = rules.get_users(guild_id=1234, channel_id=1)
+    users = rules.get_users(guild_id=1)
     assert len(users) == 3
     assert {user.user_id for user in users} == {1, 2, 3}
 
-    users = rules.get_users(guild_id=1234, channel_id=3)
+    users = rules.get_users(guild_id=3)
     assert len(users) == 0
 
 
 def test_cached_get_config(tmp_file: str) -> None:
     rules = Rules(db_path=tmp_file)
 
-    rules.update_config(CHANNEL_CONFIG._replace(guild_id=1, channel_id=1))
-    rules.get_config_cached(guild_id=1, channel_id=1)
-    assert rules.get_config_cached.cache_info().misses == 1
-    assert rules.get_config_cached.cache_info().hits == 0
+    rules.update_config(GUILD_CONFIG._replace(guild_id=1))
+    rules.get_config(guild_id=1)
+    assert rules.get_config.cache_info().misses == 1
+    assert rules.get_config.cache_info().hits == 0
 
-    rules.get_config_cached(guild_id=1, channel_id=1)
-    assert rules.get_config_cached.cache_info().misses == 1
-    assert rules.get_config_cached.cache_info().hits == 1
+    rules.get_config(guild_id=1)
+    assert rules.get_config.cache_info().misses == 1
+    assert rules.get_config.cache_info().hits == 1
 
 
 def test_cached_get_user(tmp_file: str) -> None:
     rules = Rules(db_path=tmp_file)
 
-    rules.update_user(
-        USER_OFFENSES._replace(guild_id=1, channel_id=1, user_id=1),
-    )
-    rules.get_user_cached(guild_id=1, channel_id=1, user_id=1)
-    assert rules.get_user_cached.cache_info().misses == 1
-    assert rules.get_user_cached.cache_info().hits == 0
+    rules.update_user(USER_OFFENSES._replace(guild_id=1, user_id=1))
+    rules.get_user(guild_id=1, user_id=1)
+    assert rules.get_user.cache_info().misses == 1
+    assert rules.get_user.cache_info().hits == 0
 
-    rules.get_user_cached(guild_id=1, channel_id=1, user_id=1)
-    assert rules.get_user_cached.cache_info().misses == 1
-    assert rules.get_user_cached.cache_info().hits == 1
+    rules.get_user(guild_id=1, user_id=1)
+    assert rules.get_user.cache_info().misses == 1
+    assert rules.get_user.cache_info().hits == 1
 
 
 def test_cached_get_users(tmp_file: str) -> None:
     rules = Rules(db_path=tmp_file)
 
-    rules.update_user(
-        USER_OFFENSES._replace(guild_id=1, channel_id=1, user_id=1),
-    )
-    rules.get_users_cached(guild_id=1, channel_id=1)
-    assert rules.get_users_cached.cache_info().misses == 1
-    assert rules.get_users_cached.cache_info().hits == 0
+    rules.update_user(USER_OFFENSES._replace(guild_id=1, user_id=1))
+    rules.get_users(guild_id=1)
+    assert rules.get_users.cache_info().misses == 1
+    assert rules.get_users.cache_info().hits == 0
 
-    rules.get_users_cached(guild_id=1, channel_id=1)
-    assert rules.get_users_cached.cache_info().misses == 1
-    assert rules.get_users_cached.cache_info().hits == 1
+    rules.get_users(guild_id=1)
+    assert rules.get_users.cache_info().misses == 1
+    assert rules.get_users.cache_info().hits == 1
 
 
 def test_update_config_resets_cache(tmp_file: str) -> None:
     rules = Rules(db_path=tmp_file)
 
-    rules.update_config(CHANNEL_CONFIG._replace(guild_id=1, channel_id=1))
-    rules.get_config_cached(guild_id=1, channel_id=1)
-    assert rules.get_config_cached.cache_info().misses == 1
-    assert rules.get_config_cached.cache_info().hits == 0
+    rules.update_config(GUILD_CONFIG._replace(guild_id=1))
+    rules.get_config(guild_id=1)
+    assert rules.get_config.cache_info().misses == 1
+    assert rules.get_config.cache_info().hits == 0
 
-    rules.get_config_cached(guild_id=1, channel_id=1)
-    assert rules.get_config_cached.cache_info().misses == 1
-    assert rules.get_config_cached.cache_info().hits == 1
+    rules.get_config(guild_id=1)
+    assert rules.get_config.cache_info().misses == 1
+    assert rules.get_config.cache_info().hits == 1
 
-    rules.update_config(CHANNEL_CONFIG._replace(guild_id=1, channel_id=2))
+    rules.get_configs()
+    assert rules.get_configs.cache_info().misses == 1
+    assert rules.get_configs.cache_info().hits == 0
 
-    rules.get_config_cached(guild_id=1, channel_id=1)
-    assert rules.get_config_cached.cache_info().misses == 1
-    assert rules.get_config_cached.cache_info().hits == 0
+    rules.get_configs()
+    assert rules.get_configs.cache_info().misses == 1
+    assert rules.get_configs.cache_info().hits == 1
+
+    config = GUILD_CONFIG._replace(guild_id=1, max_offenses=42)
+    rules.update_config(config)
+
+    rules.get_config(guild_id=1)
+    assert rules.get_config.cache_info().misses == 1
+    assert rules.get_config.cache_info().hits == 0
+
+    rules.get_configs()
+    assert rules.get_configs.cache_info().misses == 1
+    assert rules.get_configs.cache_info().hits == 0
 
 
 def test_update_user_resets_caches(tmp_file: str) -> None:
     rules = Rules(db_path=tmp_file)
 
-    rules.update_user(
-        USER_OFFENSES._replace(guild_id=1, channel_id=1, user_id=1),
-    )
-    rules.get_user_cached(guild_id=1, channel_id=1, user_id=1)
-    assert rules.get_user_cached.cache_info().misses == 1
-    assert rules.get_user_cached.cache_info().hits == 0
+    rules.update_user(USER_OFFENSES._replace(guild_id=1, user_id=1))
+    rules.get_user(guild_id=1, user_id=1)
+    assert rules.get_user.cache_info().misses == 1
+    assert rules.get_user.cache_info().hits == 0
 
-    rules.get_user_cached(guild_id=1, channel_id=1, user_id=1)
-    assert rules.get_user_cached.cache_info().misses == 1
-    assert rules.get_user_cached.cache_info().hits == 1
+    rules.get_user(guild_id=1, user_id=1)
+    assert rules.get_user.cache_info().misses == 1
+    assert rules.get_user.cache_info().hits == 1
 
-    rules.get_users_cached(guild_id=1, channel_id=1)
-    assert rules.get_users_cached.cache_info().misses == 1
-    assert rules.get_users_cached.cache_info().hits == 0
+    rules.get_users(guild_id=1)
+    assert rules.get_users.cache_info().misses == 1
+    assert rules.get_users.cache_info().hits == 0
 
-    rules.get_users_cached(guild_id=1, channel_id=1)
-    assert rules.get_users_cached.cache_info().misses == 1
-    assert rules.get_users_cached.cache_info().hits == 1
+    rules.get_users(guild_id=1)
+    assert rules.get_users.cache_info().misses == 1
+    assert rules.get_users.cache_info().hits == 1
 
     # Updating users should invalidate caches
-    rules.update_user(
-        USER_OFFENSES._replace(guild_id=1, channel_id=1, user_id=2),
-    )
+    rules.update_user(USER_OFFENSES._replace(guild_id=1, user_id=2))
 
-    rules.get_user_cached(guild_id=1, channel_id=1, user_id=1)
-    assert rules.get_user_cached.cache_info().misses == 1
-    assert rules.get_user_cached.cache_info().hits == 0
+    rules.get_user(guild_id=1, user_id=1)
+    assert rules.get_user.cache_info().misses == 1
+    assert rules.get_user.cache_info().hits == 0
 
-    rules.get_users_cached(guild_id=1, channel_id=1)
-    assert rules.get_users_cached.cache_info().misses == 1
-    assert rules.get_users_cached.cache_info().hits == 0
+    rules.get_users(guild_id=1)
+    assert rules.get_users.cache_info().misses == 1
+    assert rules.get_users.cache_info().hits == 0
--- threepseat/rules/data.py
+++ threepseat/rules/data.py
@@ -12,29 +12,42 @@ from threepseat.database import named_tuple_parameters
 from threepseat.database import named_tuple_parameters_update
 
 
-class ChannelConfig(NamedTuple):
-    """Row in channel config database table."""
+class GuildConfig(NamedTuple):
+    """Row in Guild config database table."""
 
     guild_id: int
-    channel_id: int
+    """Guild this config is for."""
+    enabled: int
+    """Events enabled flag."""
     event_expectancy: float
+    """Expected number of events per day."""
     event_duration: float
+    """Event duration in minutes."""
     event_cooldown: float
-    last_event: int
+    """Event cooldown in minutes (not used)."""
+    last_event: float
+    """Unix timestamp of last event occurrence (0 for no occurrences)."""
     max_offenses: int
-    timeout_duration: int
+    """Max offenses a member can get."""
+    timeout_duration: float
+    """Timeout in minutes when a user exceeds max_offenses."""
     prefixes: str
+    """Comma separated list of allowed prefixes (case-insensitive)."""
 
 
 class UserOffenses(NamedTuple):
     """Row in user offenses database table."""
 
     guild_id: int
-    channel_id: int
+    """Guild the user got the offenses in."""
     user_id: int
+    """User id with the offenses."""
     current_offenses: int
+    """Current number of offenses in the guild."""
     total_offenses: int
-    last_offense: int
+    """Lifetime number of offenses in the guild."""
+    last_offense: float
+    """Unix timestamp of last offense in the guild."""
 
 
 class Rules:
@@ -51,16 +64,13 @@ class Rules:
         if len(os.path.dirname(self.db_path)) > 0:
             os.makedirs(os.path.dirname(self.db_path), exist_ok=True)
 
-        self.get_config_cached = functools.lru_cache(maxsize=None)(
-            self.get_config,
-        )
-        self.get_user_cached = functools.lru_cache(maxsize=None)(self.get_user)
-        self.get_users_cached = functools.lru_cache(maxsize=None)(
-            self.get_users,
-        )
+        self.get_config = functools.lru_cache(maxsize=None)(self._get_config)
+        self.get_configs = functools.lru_cache(maxsize=None)(self._get_configs)
+        self.get_user = functools.lru_cache(maxsize=None)(self._get_user)
+        self.get_users = functools.lru_cache(maxsize=None)(self._get_users)
 
         with self.connect() as db:
-            create_table(db, 'channel_configs', ChannelConfig)
+            create_table(db, 'guild_configs', GuildConfig)
             create_table(db, 'user_offenses', UserOffenses)
 
     @contextlib.contextmanager
@@ -70,82 +80,75 @@ class Rules:
             with db:
                 yield db
 
-    def get_config(
-        self,
-        guild_id: int,
-        channel_id: int,
-    ) -> ChannelConfig | None:
-        """Get channel config matching guild and channel id."""
+    def _get_config(self, guild_id: int) -> GuildConfig | None:
+        """Get configuration for guild."""
         with self.connect() as db:
             rows = db.execute(
-                'SELECT * FROM channel_configs '
-                'WHERE guild_id = :guild_id AND channel_id = :channel_id',
-                {'guild_id': guild_id, 'channel_id': channel_id},
+                'SELECT * FROM guild_configs WHERE guild_id = :guild_id',
+                {'guild_id': guild_id},
             ).fetchall()
             if len(rows) == 0:
                 return None
             else:
-                return ChannelConfig(*rows[0])
+                return GuildConfig(*rows[0])
 
-    def update_config(self, config: ChannelConfig) -> None:
-        """Update channel config row that matches the guild and channel id."""
+    def _get_configs(self) -> list[GuildConfig]:
+        """Get all guild configs."""
+        with self.connect() as db:
+            rows = db.execute('SELECT * FROM guild_configs').fetchall()
+            return [GuildConfig(*row) for row in rows]
+
+    def update_config(self, config: GuildConfig) -> None:
+        """Update config row matching guild id."""
         with self.connect() as db:
             res = db.execute(
-                'UPDATE channel_configs '
-                f'SET {named_tuple_parameters_update(ChannelConfig)} '
-                'WHERE guild_id = :guild_id AND channel_id = :channel_id',
+                'UPDATE guild_configs '
+                f'SET {named_tuple_parameters_update(GuildConfig)} '
+                'WHERE guild_id = :guild_id',
                 config._asdict(),
             )
             if res.rowcount == 0:
                 res = db.execute(
-                    'INSERT INTO channel_configs VALUES '
-                    f'{named_tuple_parameters(ChannelConfig)}',
+                    'INSERT INTO guild_configs VALUES '
+                    f'{named_tuple_parameters(GuildConfig)}',
                     config._asdict(),
                 )
-            if res.rowcount > 0:
-                self.get_config_cached.cache_clear()
+            self.get_config.cache_clear()
+            self.get_configs.cache_clear()
 
-    def get_user(
+    def _get_user(
         self,
         guild_id: int,
-        channel_id: int,
         user_id: int,
     ) -> UserOffenses | None:
-        """Get the user offenses matching the guild, channel, and user id."""
+        """Get the user offenses matching the guild and user id."""
         with self.connect() as db:
             rows = db.execute(
                 'SELECT * FROM user_offenses '
-                'WHERE guild_id = :guild_id AND channel_id = :channel_id '
-                'AND user_id = :user_id',
-                {
-                    'guild_id': guild_id,
-                    'channel_id': channel_id,
-                    'user_id': user_id,
-                },
+                'WHERE guild_id = :guild_id AND user_id = :user_id',
+                {'guild_id': guild_id, 'user_id': user_id},
             ).fetchall()
             if len(rows) == 0:
                 return None
             else:
                 return UserOffenses(*rows[0])
 
-    def get_users(self, guild_id: int, channel_id: int) -> list[UserOffenses]:
-        """Get all user offenses matching a guild and channel id."""
+    def _get_users(self, guild_id: int) -> list[UserOffenses]:
+        """Get all user offenses in a guild."""
         with self.connect() as db:
             rows = db.execute(
-                'SELECT * FROM user_offenses '
-                'WHERE guild_id = :guild_id AND channel_id = :channel_id',
-                {'guild_id': guild_id, 'channel_id': channel_id},
+                'SELECT * FROM user_offenses WHERE guild_id = :guild_id',
+                {'guild_id': guild_id},
             ).fetchall()
             return [UserOffenses(*row) for row in rows]
 
     def update_user(self, user: UserOffenses) -> None:
-        """Update user offenses row matching the guild, channel, user id."""
+        """Update user offenses row matching the guild and user id."""
         with self.connect() as db:
             res = db.execute(
                 'UPDATE user_offenses '
                 f'SET {named_tuple_parameters_update(UserOffenses)} '
-                'WHERE guild_id = :guild_id AND channel_id = :channel_id '
-                'AND user_id = :user_id',
+                'WHERE guild_id = :guild_id AND user_id = :user_id',
                 user._asdict(),
             )
             if res.rowcount == 0:
@@ -154,6 +157,5 @@ class Rules:
                     f'{named_tuple_parameters(UserOffenses)}',
                     user._asdict(),
                 )
-            if res.rowcount > 0:
-                self.get_user_cached.cache_clear()
-                self.get_users_cached.cache_clear()
+            self.get_user.cache_clear()
+            self.get_users.cache_clear()
