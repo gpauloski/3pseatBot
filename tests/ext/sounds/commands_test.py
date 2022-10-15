8cdd8bb6fc73b97921050b73eca813d23d5ed9d4
Greg Pauloski <18683347+gpauloski@users.noreply.github.com>
2022-10-15 15:59:44 -0500

    Register voice channel entry sounds (#67)

--- tests/ext/sounds/commands_test.py
+++ tests/ext/sounds/commands_test.py
@@ -12,9 +12,11 @@ from discord import app_commands
 from testing.mock import MockGuild
 from testing.mock import MockInteraction
 from testing.mock import MockMember
+from testing.mock import MockVoiceChannel
 from testing.utils import extract
 from threepseat.bot import Bot
 from threepseat.ext.sounds.commands import SoundCommands
+from threepseat.ext.sounds.data import MemberSound
 
 
 @pytest.fixture
@@ -494,3 +496,133 @@ async def test_on_error(
     caplog.set_level(logging.ERROR)
     await sounds.on_error(interaction, app_commands.AppCommandError('test1'))
     assert any(['test1' in record.message for record in caplog.records])
+
+
+@pytest.mark.asyncio
+async def test_post_init(
+    sound_fixtures: tuple[Bot, SoundCommands],
+) -> None:
+    mockbot, sounds = sound_fixtures
+
+    with mock.patch.object(mockbot, 'add_listener'):
+        await sounds.post_init(mockbot)
+    sounds._vc_leaver_task.cancel()
+
+
+@pytest.mark.asyncio
+async def test_voice_state_update(
+    sound_fixtures: tuple[Bot, SoundCommands],
+    caplog,
+) -> None:
+    mockbot, sounds = sound_fixtures
+
+    guild = MockGuild('guild', 1)
+    member = MockMember('member', 2, guild)
+
+    before = mock.MagicMock()
+    after = mock.MagicMock()
+    before.channel = None
+    after.channel = None
+
+    with mock.patch('threepseat.ext.sounds.commands.play_sound') as mock_play:
+        # skip: before and after are the same
+        await sounds.on_voice_state_update(member, before, after)
+        assert mock_play.await_count == 0
+
+        # skip: after channel is not voice channel
+        before.channel = MockVoiceChannel()
+        await sounds.on_voice_state_update(member, before, after)
+        assert mock_play.await_count == 0
+
+        # skip: member has not registered sound
+        after.channel = before.channel
+        before.channel = None
+        await sounds.on_voice_state_update(member, before, after)
+        assert mock_play.await_count == 0
+
+        # skip: sound is not in database
+        sounds.join_table.update(
+            MemberSound(member_id=2, guild_id=1, name='test', updated_time=0),
+        )
+        await sounds.on_voice_state_update(member, before, after)
+        assert mock_play.await_count == 0
+
+        # succeed
+        sounds.table.add(
+            name='test',
+            description='test',
+            link='https://youtube.com',
+            author_id=2,
+            guild_id=1,
+        )
+        await sounds.on_voice_state_update(member, before, after)
+        assert mock_play.await_count == 1
+
+    with mock.patch(
+        'threepseat.ext.sounds.commands.play_sound',
+        side_effect=Exception(),
+    ):
+        # exception should be captured and logged
+        await sounds.on_voice_state_update(member, before, after)
+
+    caplog.set_level(logging.ERROR)
+    assert any(['exception' in record.message for record in caplog.records])
+
+
+@pytest.mark.asyncio
+async def test_register_command_sound_missing(
+    sound_fixtures: tuple[Bot, SoundCommands],
+) -> None:
+    mockbot, sounds = sound_fixtures
+    register_ = extract(sounds.register)
+
+    interaction = MockInteraction(
+        sounds.register,
+        user='calling-user',
+        channel='mychannel',
+        guild='myguild',
+        client=mockbot,
+    )
+
+    await register_(sounds, interaction, name='mysound')
+
+    assert interaction.responded
+    assert (
+        interaction.response_message is not None
+        and 'does not exist' in interaction.response_message
+    )
+
+
+@pytest.mark.asyncio
+async def test_register_command(
+    sound_fixtures: tuple[Bot, SoundCommands],
+) -> None:
+    mockbot, sounds = sound_fixtures
+    register_ = extract(sounds.register)
+
+    interaction = MockInteraction(
+        sounds.register,
+        user='calling-user',
+        channel='mychannel',
+        guild='myguild',
+        client=mockbot,
+    )
+    assert interaction.guild is not None
+    assert interaction.user is not None
+
+    sounds.table.add(
+        name='mysound',
+        description='test',
+        link='https://youtube.com',
+        author_id=interaction.user.id,
+        guild_id=interaction.guild.id,
+    )
+    assert len(sounds.join_table.all(interaction.guild.id)) == 0
+    await register_(sounds, interaction, name='mysound')
+    assert len(sounds.join_table.all(interaction.guild.id)) == 1
+
+    assert interaction.response
+    assert (
+        interaction.response_message is not None
+        and 'Updated your voice channel entry' in interaction.response_message
+    )
